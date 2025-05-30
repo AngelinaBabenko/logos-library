ca4dccb33fa1eb9268a72752bd176410f3975de0
Dmitriy Kondratenko <dmitriy.kondratenko@phobosys.com>
2025-05-30 11:44:09 +0300

    Updates

---
+++ precache-manifest.d481c54eaa467b8f4953f706601f5d24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ec6ba21cd8c8c1735a4526f6cbc680",
+    "url": "/logos-library/index.html"
+  },
+  {
+    "revision": "793cb3caf92b778691b4",
+    "url": "/logos-library/static/css/main.65c6d164.chunk.css"
+  },
+  {
+    "revision": "695e54c81d68248a661b",
+    "url": "/logos-library/static/js/2.4779268e.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/logos-library/static/js/2.4779268e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "793cb3caf92b778691b4",
+    "url": "/logos-library/static/js/main.677bab26.chunk.js"
+  },
+  {
+    "revision": "99a5a0f55eebabfd73a3",
+    "url": "/logos-library/static/js/runtime-main.b9f90f1a.js"
+  }
+]);
